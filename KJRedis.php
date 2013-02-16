284380f3f1db832119c66bb200cad92eb97a52a0
Kim Jeker <info@kije.ch>
2013-02-16 18:36:05 +0100

    (Hopefully)Improved Exception handling and added 'call redirection' to the Redis() class

--- KJRedis.php
+++ KJRedis.php
@@ -1,11 +1,12 @@
 <?php
 
+require_once 'CustomException.php';
 
 /**
  * Requires installed phpredis (phpmodule)! https://github.com/nicolasff/phpredis
  */
 
-class KJException extends Exception {}
+class KJException extends CustomException {}
 
 class KJRedis {
 	/**
@@ -16,9 +17,10 @@ class KJRedis {
 
 	/**
 	 * 
-	 * @param String $host 		host or soket to connect to redis
+	 * @param String $host 		host or unix soket file to connect to redis
 	 * @param int 	 $port 		port to connect to redis (optional)
 	 * @param double $timeout 	timeout for the connection (optional)
+	 * @throws KJException If $args > 3
 	 */
 	public function __construct($host /*, $port, $timeout*/) {
 		$args = func_get_args();
@@ -44,7 +46,7 @@ class KJRedis {
 			}
 		} catch (KJException $e) {
 			echo $e->getMessage();
-			exit(); // exit script to avoid more errors
+			exit(); 
 
 		} catch (RedisException $e) {
 			echo 'RedisException caught: ' . $e->getMessage();
@@ -52,14 +54,61 @@ class KJRedis {
 		}
 	}
 
-	public function get($key) {
-		return $this->redis->get($key);
+	public function __call($name, $arguments) {
+	 	return call_user_func_array(array($this->redis, $name), $arguments); 
 	}
 
-	public function set($key, $value) {
+	/**
+	 * Set a value for key. Optional you can provide an *Associative array*, then this methode will set for every key => value pair the corresponding value for the key. On Error, it throws a KJException
+	 * @param String $key 	the key
+	 * @param String $value the value to set
+	 * @return KJRedis 		Returs an KJRedis-Object so you can use following syntax: $kjredis->set('text', 1)->set('status', 'active')->set('test', 'works'); 
+	 * @throws KJException If $args > 2
+	 */
+	public function set(/*$key, $value || $asoc_array*/) {
+		try {
+			if (func_num_args() == 1 && is_array(func_get_arg(0))) {
+				foreach (func_get_arg(0) as $key => $value) {
+					$this->redis->set($key, $value);
+				} else if (func_num_args() == 2) {
+					$this->redis->set(func_get_arg(0), func_get_arg(1));
+				}
+				else {
+					throw new KJException('Wrong number of arguments: ' . func_num_args() . ' given, max. 2 expected!\n<br />{$e}' );
+				}
+			} 
+		catch (KJException $e) {
+			echo $e->getMessage();
+			return false;
+		}
 
-		$this->redis->get($key, $value);
+		return $this; 
+	}
 
-		return $this; // to allow following sytax $kjredis->set('text', 1)_>set('status', 'active')->set('test', 'works'); 
+	/**
+	 * Set a value for key, if it not exists. Optional you can provide an *Associative array*, then this methode will set for every key => value pair the corresponding value for the key. On Error, it throws a KJException
+	 * @param String $key 			the key
+	 * @param String $value 		the value to set
+	 * @return KJRedis || bool 		Returs on succes an KJRedis-Object so you can use following syntax: $kjredis->set('text', 1)->set('status', 'active')->set('test', 'works'); If the $key => $value pair allready exist in Redis, this methode will return false
+	 * @throws KJException If $args > 2
+	 */
+	public function setnx(/*$key, $value || $asoc_array*/) {
+		try {
+			$ret = true;
+			if (func_num_args() == 1 && is_array(func_get_arg(0))) {
+				foreach (func_get_arg(0) as $key => $value) {
+						$ret = $this->redis->setnx($key, $value);
+				}
+			} else if (func_num_args() == 2) {
+					$ret = $this->redis->setnx(func_get_arg(0), func_get_arg(1));
+			} else {
+					throw new KJException('Wrong number of arguments: ' . func_num_args() . ' given, max. 2 expected!\n<br />{$e}' );
+			}
+		} catch (KJException $e) {
+			echo $e->getMessage();
+			return false;
+		}
+
+		return ($ret ? $this : false);
 	}
 }
